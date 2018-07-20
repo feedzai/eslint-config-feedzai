8b9954142314c2a55d10f52b0892c865814178e4
Victor Fernandes <victorfern91@gmail.com>
2018-07-20 16:52:24 +0100

    Add React rules (#4)

---
+++ eslint-config-feedzai-react/rules/jest.js
@@ -0,0 +1,18 @@
+/**
+ * Jest Rules
+ *
+ * @author Victor Fernandes (victor.fernandes@feedzai.com)
+ * @since 1.0.0
+ */
+
+module.exports = {
+    env: {
+        "jest/globals": true
+    },
+
+    plugins: ["jest"],
+
+    extends: "plugin:jest/recommended",
+
+    rules: {}
+};
